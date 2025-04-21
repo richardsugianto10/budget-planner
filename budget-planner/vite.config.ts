1a6102f290ed8218cce247c2648427a8cacdf46b
Richard_023614 <richard.sugianto@banksinarmas.com>
2025-04-21 22:02:17 +0700

    Deploy to production

--- budget-planner/vite.config.ts
+++ budget-planner/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bidget-planner/',
+  base: '/budget-planner/',
   plugins: [
     vue(),
     vueDevTools(),
